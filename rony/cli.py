6e9ca945c45a48e1d1e0e1e41333671c4fb6c633
Neylson Crepalde <neylsoncrepalde@gmail.com>
2021-01-05 13:52:15 -0300

    docker build and run on CLI

--- rony/cli.py
+++ rony/cli.py
@@ -51,5 +51,41 @@ def new(project_name, implemented):
     os.makedirs(os.path.join(LOCAL_PATH, project_name, 'tests'))
     os.makedirs(os.path.join(LOCAL_PATH, project_name, '.github/workflows'))
 
+    # Write files
+    # ...
+
+    print(f'Creating virtual environment {project_name}_env')
+    os.chdir(project_name)
+    env_name = f"{project_name}_env"
+    os.system(f"python -m venv {env_name}")
+
+    # Create git repo
+    os.system('git init')
+    print("A git repository was created. You should add your files and make your first commit.\n")
     
-    
+
+
+@click.argument('image_name')
+@cli.command()
+def build(image_name):
+    """
+    Build a docker image with given image_name. Only run if you have docker installed.
+    One should be at the root directory.
+    """
+    if not os.path.exists('Dockerfile'):
+        click.echo("You gotta have a Dockerfile file.")
+    else:
+        os.system(f'docker build -t {image_name} .')
+
+
+@click.argument('image_name')
+@cli.command()
+def run(image_name):
+    """
+    Run a container with given image_name. 
+    Only run if you have docker installed.
+    """
+    if not os.path.exists('Dockerfile'):
+        click.echo("You gotta have a Dockerfile file")
+    else:
+        os.system(f'docker run --rm -p 5000:5000 {image_name}')

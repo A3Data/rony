7fbc2a313aa6c1963c9bcd7a70c688a61d7ec803
Carlos Barbosa <carlosdspy@gmail.com>
2021-01-15 15:52:47 -0300

    fix: adds the environment ENV_NAME variable to capture in the session

--- rony/cli.py
+++ rony/cli.py
@@ -2,8 +2,8 @@ import click
 import os
 import re
 import sys
-from os import path
 from .writer import copy_files, write_readme_file
+from .validation import get_operational_system, check_version_python, check_python_compile
 from .__init__ import __version__ as version
 
 LOCAL_PATH = os.getcwd()
@@ -50,61 +50,14 @@ def new(project_name):
     write_readme_file(LOCAL_PATH, project_name)
 
     os.chdir(project_name)
-    new.env_name = f"{project_name}_env"
+    env_name = f"{project_name}_env"
+    os.environ["ENV_NAME"] = env_name
+    check_python_compile()
 
     # Create git repo
     os.system('git init')
     print("A git repository was created. You should add your files and make your first commit.\n")
 
-
-env_name = new.env_name    
-    
-def get_operational_system():
-    """
-    Check which user's operating system    
-    """
-    os = sys.platform
-    if sys.platform == "linux":
-        return "Linux"
-    elif sys.platform == "win32":
-        return "Windows"
-    elif sys.platform == "darwin":
-        return "MacOS"
-    elif sys.platform == "freebsd8":
-        return "FreeBSD"
-
-
-def check_version_python():
-    """
-    Checks whether the version of python installed on the system is compatible
-    """
-    if sys.version_info >= (3,6):
-        return True
-    else:
-        return False
-
-def check_python_compile():
-    """
-    Checks python3 path location 
-    """
-    message_env = f"Creating virtual environment {env_name}"
-    if get_operational_system() == "Linux" and check_version_python() == True:
-        if path.exists("/usr/bin/python3") == True:
-            print(message_env)
-            os.system(f"python3 -m venv {env_name}")
-        else:
-            print(message_env)
-            os.system(f"python -m venv {env_name}")
-    elif get_operational_system() == "Windows" and check_version_python() == True:
-        print(message_env)
-        os.system(f"python -m venv {env_name}")
-    elif get_operational_system() == "MacOS" and check_version_python() == True:
-        print(message_env)
-        os.system(f"python -m venv {env_name}")
-    elif check_version_python == False:
-        print('The python version is not supported. Rony is compatible with the version of >= Python 3.6')
-
-check_python_compile()
 
 @click.argument('image_name')
 @cli.command()

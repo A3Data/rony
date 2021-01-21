c26468e37e39c2d22f256a7887fd60984decb521
Neylson Crepalde <neylsoncrepalde@gmail.com>
2021-01-21 13:00:06 -0300

    correct typos and workdir to infrastructure

--- rony/base_files/scripts/build_lambda_package.sh
+++ rony/base_files/scripts/build_lambda_package.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash 
 
+cd infrastructure
+
 # Declara variavel para reutilização nas validações do diretório
 PACKAGE="package"
 
 # Cria o diretório e instala as dependências da função lambda
-if [[ -d $PACKAGE ]]
+if [ -d $PACKAGE ]
 then
 	echo "O Diretório "$PACKAGE" já existe."
 else
@@ -19,7 +21,7 @@ fi
 FILE_REQUIREMENTS=../etl/lambda_requirements.txt
 
 # Verifica se o arquivo lambda_requirements existe
-if [[ -f $FILE_REQUIREMENTS ]]
+if [ -f $FILE_REQUIREMENTS ]
 then
 	echo "============================================="
 	echo "Instalando dependências localizadas no "$FILE_REQUIREMENTS""
@@ -32,16 +34,16 @@ fi
 cd $PACKAGE
 
 # Declara variavel que localiza a função lambda para reutilização no código.
-LAMBDA_FUNCTION=../etl/lambda_function.py
+LAMBDA_FUNCTION=../../etl/lambda_function.py
 
 # Verifica se o arquivo lambda_function.py existe
-if [[ -f $LAMBDA_FUNCTION ]]
+if [ -f $LAMBDA_FUNCTION ]
 then
 	echo "============================================="
 	echo "Copiando função Handler..."
-	cp ../etl/lambda_function.py .
+	cp $LAMBDA_FUNCTION .
 	echo "Compactando arquivo lambda_function_payload.zip"
-	zip -r9 ../infrastructure/lambda_function_payload.zip . #Compacta o pacote para o deploy
+	zip -r9 ../lambda_function_payload.zip . #Compacta o pacote para o deploy
 	echo "Arquivo compactado com sucesso!"
 	echo "============================================="
 fi

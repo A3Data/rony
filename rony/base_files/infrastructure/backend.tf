0a09d99132aa50dbe39745ca0c4ef52fc19c4f44
filipe <filipelima@lett.digital>
2021-02-05 11:08:50 -0300

    comment remote state s3

--- rony/base_files/infrastructure/backend.tf
+++ rony/base_files/infrastructure/backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-bucket-state-backend"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
+//terraform {
+//  backend "s3" {
+//    bucket         = "my-bucket-state-backend"
+//    key            = "terraform.tfstate"
+//    region         = "us-east-1"
+//  }
+//}

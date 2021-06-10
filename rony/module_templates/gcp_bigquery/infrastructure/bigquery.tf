e1b6904fabd6f3bbbf22d56fe9612887d9990dec
westerley <westerley.reis@a3data.com.br>
2021-06-10 01:30:04 -0400

    - Correção recursos cloud function, bigquery e storage - Add recurso cloud compose

--- rony/module_templates/gcp_bigquery/infrastructure/bigquery.tf
+++ rony/module_templates/gcp_bigquery/infrastructure/bigquery.tf
@@ -2,15 +2,15 @@ resource "google_bigquery_dataset" "dataset" {
   dataset_id                  = "dataset_name"
   friendly_name               = "dataset_friendly_name"
   description                 = "Example of dataset"
-  location                    = var.location
+  location                    = var.region_id
   delete_contents_on_destroy  = true
 }
 
 resource "google_bigquery_table" "table" {
-  dataset_id          = [google_bigquery_dataset.dataset.dataset_id]
+  dataset_id          = "dataset_name"
   friendly_name       = "table_friendly_name"
   table_id            = "table_name"
-  schema              = "PATH"
+  schema              = file("../schemas/iris.json")
   description         = "Example of table"
   project             = var.project_id
   deletion_protection = false

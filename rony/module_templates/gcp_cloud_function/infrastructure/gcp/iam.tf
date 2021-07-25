fcddb537aa89deb066e324ac184657fb2b43f66b
Westerley <westerreis@gmail.com>
2021-07-25 19:08:26 -0400

    Add new module - pubsub

--- rony/module_templates/gcp_cloud_function/infrastructure/gcp/iam.tf
+++
@@ -1,9 +0,0 @@
-resource "google_cloudfunctions_function_iam_member" "invoker_fn_example_script" {
-  project        = google_cloudfunctions_function.fn_example_script.project
-  region         = google_cloudfunctions_function.fn_example_script.region
-  cloud_function = google_cloudfunctions_function.fn_example_script.name
-
-  role   = "roles/cloudfunctions.invoker"
-  member = "allUsers"
-  depends_on = [google_cloudfunctions_function.fn_example_script]
-}

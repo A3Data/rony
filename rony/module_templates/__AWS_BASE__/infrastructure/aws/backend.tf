4494466ac7a5da96e8993680c5ab3c6e8dcd519c
Rodrigo Torres <ratorres7@gmail.com>
2021-08-11 12:30:32 -0300

    Removed default bucket on terraform backend

--- rony/module_templates/__AWS_BASE__/infrastructure/aws/backend.tf
+++ rony/module_templates/__AWS_BASE__/infrastructure/aws/backend.tf
@@ -1,7 +1,7 @@
 # Backend configuration require a AWS storage bucket.
 terraform {
   backend "s3" {
-    bucket = "terraform-state-rony"
+    bucket = ""
     key    = "state/terraform.tfstate"
     region = "us-east-1"
   }

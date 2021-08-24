0d17433576aae4386eb065b7a2a91b9c6192c68a
Gabriel <gabriel.bonifacio@a3data.com.br>
2021-08-24 17:43:37 -0300

    Atualizaçao Glue Job

--- rony/module_templates/aws_glue_job/infrastructure/aws/storage.tf
+++ rony/module_templates/aws_glue_job/infrastructure/aws/storage.tf
@@ -2,13 +2,13 @@ resource "aws_s3_bucket" "bucket_glue_job" {
   bucket = "${var.prefix}-${var.bucket_glue_job}-${var.account}"
   acl    = "private"
 
-    tags = local.common_tags
+  tags = local.common_tags
 
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-       }
-     }
-   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }

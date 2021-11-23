0c3bb42137a3e57741effd67a424267bc807b648
Neylson Crepalde <neylson.crepalde@a3data.com.br>
2021-11-23 08:21:06 -0300

    first DataQuality abstract Class

---
+++ rony/module_templates/data_quality/data_quality.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from pyspark.sql import SparkSession
+
+
+class DataQuality(ABC):
+    """
+    Abstract DataQuality Class
+    """
+
+    @abstractmethod
+    def __init__(self, spark: SparkSession) -> None:
+        self.spark = spark
+
+
+    @abstractmethod
+    def write_output(self, dataframe: bool = True) -> None:
+        """
+        Write output for DataQuality process.
+
+        Parameters
+        ----------
+
+        dataframe: bool = True
+            if True, results are returned as a spark DataFrame. If False,
+            results are returned as a dict object.
+        """
+        if dataframe:
+            pass
+        else:
+            pass
+

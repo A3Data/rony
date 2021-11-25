2840cb656ce3f418d00c40ad54aa8b2220989c8e
Neylson Crepalde <neylson.crepalde@a3data.com.br>
2021-11-24 22:11:07 -0300

    Analyzer with config file or string expression working. Only one column methods yet

--- rony/data_quality/analyzer.py
+++ rony/data_quality/analyzer.py
@@ -1,9 +1,15 @@
+import json
 from pyspark.sql.dataframe import DataFrame
 from .data_quality import DataQuality
 from pyspark.sql import SparkSession, DataFrame
 from pydeequ.analyzers import (
-    AnalysisRunner, AnalyzerContext, Size, Completeness, Mean,
-    Correlation, ApproxCountDistinct
+    AnalysisRunner, AnalyzerContext,
+    ApproxCountDistinct, ApproxQuantile, ApproxQuantiles,
+    Completeness, Compliance, Correlation, CountDistinct,
+    DataType, Distinctness, Entropy, Histogram, KLLParameters, KLLSketch,
+    Maximum, MaxLength, Mean, Minimum, MinLength, MutualInformation,
+    PatternMatch, Size, StandardDeviation, Sum, Uniqueness,
+    UniqueValueRatio
 )
 
 
@@ -21,7 +27,39 @@ class Analyzer(DataQuality):
         super().__init__(spark)
 
 
-    def run(self, df: DataFrame) -> DataFrame:
+    def _analysis_job_builder(self, config_path: str) -> str:
+        """
+        WIP - For now, only works with single column methods.
+        Build the Analyzer job code as a string expression to be evaluated.
+
+        Parameters
+        ----------
+        config_path: str
+            Path to a json config file.
+            Config file should have column names as keys and lists of
+            analysis methods as values.
+
+            Example:
+            {"PassengerId": ["Completeness"],
+            "Age": ["Mean", "StandardDeviation", "Minimum", "Maximum"]}
+
+        Returns
+        -------
+        str -> The AnalysisRunner expression as a str object
+        """
+        with open(config_path, "r") as file:
+            configurations = json.load(file)
+
+        expression = "AnalysisRunner(spark).onData(df).addAnalyzer(Size())"
+
+        for col in configurations.keys():
+            for method in configurations[col]:
+                expression += ".addAnalyzer(" + method + '("' + col + '"))'
+        expression += ".run()"
+        return expression
+
+
+    def run(self, df: DataFrame, config: str) -> DataFrame:
         """
         Run the DataQuality job
 
@@ -31,21 +69,26 @@ class Analyzer(DataQuality):
         df: DataFrame
             A Spark DataFrame to run DataQuality jobs.
 
+        config: str
+            Path to a json config file or a str expression of AnalysisRunner
+            to evaluate
+
         Returns
         -------
 
         DataFrame -> A DataFrame with the results for DataQuality job.
         """
-        analysisResult = (
-            AnalysisRunner(self.spark)
-                .onData(df)
-                .addAnalyzer(Size())
-                .addAnalyzer(Completeness("PassengerId"))
-                .addAnalyzer(ApproxCountDistinct("Pclass"))
-                .addAnalyzer(Mean("Age"))
-                .addAnalyzer(Correlation("Age", "Fare"))
-                .run()
-        )
+        try:
+            expression = self._analysis_job_builder(config)
+        except:
+            expression_start = "AnalysisRunner(spark).onData(df)"
+            if not config.startswith(expression_start):
+                raise AttributeError("String expression should start with 'AnalysisRunner(spark).onData(df)'")
+            else:
+                expression = config
+
+        spark = self.spark
+        analysisResult = eval(expression)
 
         analysisResult_df = (
             AnalyzerContext
